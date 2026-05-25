c7313893e711a34489fa2922fb622c987c932f91
Eazhil <eazhil2002@gmail>
2026-05-25 15:05:48 +0530

    update 1

--- auto_commit.bat
+++ auto_commit.bat
@@ -2,20 +2,23 @@
 
 cd /d C:\Users\17491\daily-commit-demo
 
-echo Updated on %date% %time% >> daily_log.txt
-echo Change 1 >> daily_log.txt
-echo Change 2 >> daily_log.txt
-echo Change 3 >> daily_log.txt
-echo Change 4 >> daily_log.txt
-echo Change 5 >> daily_log.txt
-echo Change 6 >> daily_log.txt
-echo Change 7 >> daily_log.txt
-echo Change 8 >> daily_log.txt
-echo Change 9 >> daily_log.txt
-echo Change 10 >> daily_log.txt
+setlocal enabledelayedexpansion
 
-git add .
+set /a commits=%random% %%5 +5
 
-git commit -m "Daily auto commit"
+echo Running !commits! commits...
 
-git push origin main
+for /L %%i in (1,1,!commits!) do (
+
+    echo Update %%i %date% %time% %random% >> daily_log.txt
+
+    git add .
+
+    git commit -m "update %%i"
+
+    git push origin main
+
+    timeout /t 5 /nobreak >nul
+)
+
+echo Finished.
